14bde6a365fb0ef9eb0e79a0edc02b3a908024c9
Esta Nagy <nagyesta@gmail.com>
2023-02-20 19:37:34 +0100

    Add baseline demo module (#2)
    - Creates new baseline module
    - Improves connectivity verification
    - Adds new module to workflow
    
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

---
+++ baseline-demo/src/main/java/com/github/nagyesta/example/baselinedemo/MySqlConnectionCheck.java
@@ -0,0 +1,36 @@
+package com.github.nagyesta.example.baselinedemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class MySqlConnectionCheck {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public MySqlConnectionCheck(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void verifyConnectivity() throws SQLException {
+        try (final Connection connection = dataSource.getConnection()) {
+            query(connection);
+        }
+    }
+
+    private void query(Connection connection) throws SQLException {
+        final String sql = "SELECT CONCAT(@@version_comment, ' - ', VERSION()) FROM DUAL";
+        try (final ResultSet resultSet = connection.prepareStatement(sql).executeQuery()) {
+            resultSet.next();
+            final String value = resultSet.getString(1);
+            //write something that will be visible on the Gradle output
+            System.err.println(value);
+        }
+    }
+}
